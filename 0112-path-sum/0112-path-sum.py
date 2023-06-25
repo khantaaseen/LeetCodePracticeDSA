0dda1f9ee4da31c3eb4ea4135e778afe6a4ec1ad
Taaseen Khan <91350132+khantaaseen@users.noreply.github.com>
2023-06-25 16:47:33 -0400

    Time: 55 ms (77.83%), Space: 17.3 MB (99.3%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -7,21 +7,30 @@
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         
-        if not root:
+
+        #O(N) time complexity because we have to go through every node to check total
+        #O(N) memory complexity beacause we are using the height of the tree
+        #best case memory complexity is O(logN) if this is balanced binary tree
+
+        if not root: #base case, if there are no roots, it is not equal to any sum
             return False
 
         def dfs(node, total):
 
             if not node:
-                return
+                return 
 
-            total += node.val
-            if not node.left and not node.right:
-                return total == targetSum
+            total += node.val #add the value of the current node to our total
+            if not node.left and not node.right: #executes if we are at a leaf node
+            #if the total after coming to leaf code is == sum
+                return total == targetSum 
 
-            return (dfs(node.left, total) or
+            #this return statement will return true or false and get the true or false
+            #between both sides of the tree   
+            return (dfs(node.left, total) or 
                     dfs(node.right,total))
-                    
+
+        #returning the current root, and the current total
         return dfs(root, 0)
 
 
