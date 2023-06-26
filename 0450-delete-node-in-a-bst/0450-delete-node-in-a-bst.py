a3a25c31e66afc3943e6e6b34437bdc819697f17
Taaseen Khan <91350132+khantaaseen@users.noreply.github.com>
2023-06-25 21:03:11 -0400

    Time: 82 ms (87.52%), Space: 20.6 MB (86.15%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -7,27 +7,28 @@
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
 
-        if not root:
+        if not root: #if the tree is empty, nothing to change
             return None
 
-        curr = root
-
-        if key < root.val:
+        #decides on what side of the tree we should search in
+        if key < root.val: #if key is smaller, search in left subtree
             root.left = self.deleteNode(root.left, key)
-        elif key > root.val:
+        elif key > root.val: #if key is larger, serach in right subtree
             root.right = self.deleteNode(root.right, key)
-        else:
-            if not root.left:
+        else: #at this moment we have found the key in the BST
+
+            if not root.left: #if there is no left child, the deleted node becomes right
                 return root.right 
-            elif not root.right:
+            elif not root.right: #if theres no right child, deleted node becomes left
                 return root.left
 
             #after deleting and if there are children nodes
             #go down the right subtree and find the minimum in that right tree
-            curr = root.right
-            while curr.left:
-                curr = curr.left
-            root.val = curr.val
+            curr = root.right #since we are looking for min in right tree, curr = right 
+            while curr.left: #while there are left childred (for min keep going left)
+                curr = curr.left #keep going left for min
+            root.val = curr.val #after you have found the min this becomes our curr value
+            #to get rid of dupe node after deleting, find this node and delete it
             root.right = self.deleteNode(root.right, root.val)
         return root
 
