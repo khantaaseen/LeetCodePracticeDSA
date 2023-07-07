8ce55990b985c7ef0607c4a267ea98869b97bec7
Taaseen Khan <91350132+khantaaseen@users.noreply.github.com>
2023-07-07 17:10:03 -0400

    Time: 89 ms (80.01%), Space: 20.9 MB (53.81%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -11,9 +11,12 @@ class Solution:
         curr = root
 
         while curr:
+            #if both of the vals are greater then search in right subtree
             if p.val > curr.val and q.val > curr.val:
                 curr = curr.right
+            #if both of the val are less, search in left subtree
             elif p.val < curr.val and q.val < curr.val:
                 curr = curr.left
+            #if the values are in different subtrees, the root is the LCA
             else:
                 return curr
