b527e18406f7aac7700abdffcbdcdb63e5aaa1ef
Taaseen Khan <91350132+khantaaseen@users.noreply.github.com>
2023-07-07 20:21:05 -0400

    Time: 598 ms (84.97%), Space: 32.4 MB (22.4%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,31 +1,31 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
 
-        # hashset = set()
+        hashset = set()
 
-        # for i in nums:
-        #     if i in hashset:
-        #         return i
-        #     else:
-        #         hashset.add(i)
-        # return None
+        for i in nums:
+            if i in hashset:
+                return i
+            else:
+                hashset.add(i)
+        return None
 
 
         #or floydes algorithm of fast and slow pointers
 
-        slow, fast = 0, 0
+        # slow, fast = 0, 0
 
-        while True:
-            slow = nums[slow]
-            fast = nums[nums[fast]]
-            if slow == fast:
-                break 
+        # while True:
+        #     slow = nums[slow]
+        #     fast = nums[nums[fast]]
+        #     if slow == fast:
+        #         break 
 
-        slow2 = 0
-        while True:
-            slow = nums[slow]
-            slow2 = nums[slow2]
-            if slow == slow2:
-                return slow
+        # slow2 = 0
+        # while True:
+        #     slow = nums[slow]
+        #     slow2 = nums[slow2]
+        #     if slow == slow2:
+        #         return slow
             
 
