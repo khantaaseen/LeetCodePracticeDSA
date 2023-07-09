8adff9f678c0d57274f011b0edf1ff7e84b519d6
Taaseen Khan <91350132+khantaaseen@users.noreply.github.com>
2023-07-08 20:35:34 -0400

    Time: 167 ms (75.42%), Space: 31.6 MB (69.56%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -7,40 +7,47 @@
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
 
-        # if not headA or not headB:
-        #     return None
+        if not headA or not headB:
+            return None
 
 
-        # currA = headA
-        # currB = headB
+        #essentially this algorithm assigns two pointers to run through each of the linked lists
+        #if one of the curr nodes is null before the other curr node we begin curr node at head
+        #of the opposite list and same for the other node, doing this will cause each curr to be 
+        #at the same node if there is an intersection because the pointers will begin to match 
+        #up in speed
 
-        # while currA != currB:
-        #     if currA:
-        #         currA = currA.next
-        #     else:
-        #         currA = headB
+        #two pointer approach similar to floydes tortoise and hare algorithm
+        currA = headA
+        currB = headB
 
-        #     if currB:
-        #         currB = currB.next
-        #     else:
-        #         currB = headA
-        # return currA
+        while currA != currB: #if values are same, intersect found and return either val
+            if currA: #while not null keep going
+                currA = currA.next
+            else: #when this becomes null reassign pointer to beginning of second list
+                currA = headB
+
+            if currB: #while not null keep going 
+                currB = currB.next
+            else: #when becomes null assign this to other list until they are equal
+                currB = headA
+        return currA #when equal you can return either list val
 
         
-        hashset = set()
-        curr = headA
+        # hashset = set()
+        # curr = headA
         
-        while curr:
-            hashset.add(curr)
-            curr = curr.next
+        # while curr:
+        #     hashset.add(curr)
+        #     curr = curr.next
         
-        curr = headB
-        while curr:
-            if curr in hashset:
-                return curr
-            curr = curr.next
+        # curr = headB
+        # while curr:
+        #     if curr in hashset:
+        #         return curr
+        #     curr = curr.next
 
-        return None
+        # return None
 
 
 
