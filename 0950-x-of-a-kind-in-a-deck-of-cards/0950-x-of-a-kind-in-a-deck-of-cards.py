05455c17bf6c496722a514df5529857a17d94486
Taaseen Khan <91350132+khantaaseen@users.noreply.github.com>
2023-07-09 20:08:38 -0400

    Time: 151 ms (29.26%), Space: 16.9 MB (11.04%) - LeetHub

---
+++ 0950-x-of-a-kind-in-a-deck-of-cards/0950-x-of-a-kind-in-a-deck-of-cards.py
@@ -0,0 +1,31 @@
+class Solution:
+    def hasGroupsSizeX(self, deck: List[int]) -> bool:
+
+        cards = Counter(deck)
+        print(deck)
+        print(cards)
+        w = list(set(cards.values()))
+        print(set(cards.values()))
+        print(w)
+        div = 0
+
+        if len(w) == 1:
+            div = w[0]
+        else:
+            div = gcd(w[0], w[1])
+            print(div)
+
+            for i in range(1, len(w)-1):
+                div = gcd(div, w[i + 1])
+        return div > 1
+
+        # for card in deck:
+        #     if deck[card] in cards:
+        #         cards[card] = 1 + cards.get(deck[card], 0)
+        #     else:
+        #         cards[card] = 1
+        
+        # for i in cards:
+        #     if cards[i] < 1:
+        #         return False
+        # return True
